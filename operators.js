4c2e7584d5fe69f01e8321967f668f95a487c785
SankalpaJ <sankalpaj977@gmail.com>
2025-02-06 15:22:37 +0530

    oper1

---
+++ operators.js
@@ -0,0 +1,17 @@
+console.log(10 + 20);
+console.log(10 - 20);
+console.log(10 * 20);
+console.log(2 ** 3); // 8
+
+//var a = 10;
+//console.log(a++); // post incremet -- 10 but internal a is 11
+//console.log(a);   // a = 11
+//console.log(++a); // pre incremt -- 12
+
+// checks only value
+//console.log(10 == 10); // true
+//console.log(10 == '10'); // true
+
+// checks value and data type 
+//console.log(10 === 10); 
+
