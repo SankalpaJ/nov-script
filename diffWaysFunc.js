2f2be2e93115c32f3933cf2be765dc41aed6eef8
SankalpaJ <sankalpaj977@gmail.com>
2025-02-24 19:48:07 +0530

    added diff_ways_of_func

---
+++ diffWaysFunc.js
@@ -0,0 +1,31 @@
+// function Declaration: 
+
+function add(a,b){
+    console.log(a+b);
+}
+add(100,200);
+
+// function expession 
+// here function() r block z completly assigned to sub variable, 
+//  so insted of console.log we can directly we call sub().....
+const sub = function(a,b) {
+    console.log(a-b);
+}
+sub(100, 20);
+
+// Arrow function 
+
+const mul = (a,b) => {
+    console.log(a * b);
+}
+mul(10, 20);
+
+// when ever we have 1 statement to return their z no need to use of braces...
+
+//const msg = ()=> "Hello World...";
+//let res = msg();
+//console.log(res);
+
+const msg = ()=> {return "Hello world..."}
+let res = msg();
+console.log(res);
