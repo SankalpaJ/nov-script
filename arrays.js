ef104043f9127cbdd1b416f006e6356d94b1ca41
SankalpaJ <sankalpaj977@gmail.com>
2025-02-25 15:52:41 +0530

    Object Files

---
+++ arrays.js
@@ -0,0 +1,17 @@
+// var arr1 = [10, 20, 30, 40,50,60,70]
+// console.log(arr1.length);
+
+// for(var i = 0; i <= arr1.length-1; i++) {
+//    console.log('Element at index ${i}', arr1[i]);
+//  }
+
+var arr2 = [10, 20.3456, 'kodnest', {name: 'Priya', age: 20}, ()=> {console.log('Funtion')}, [100, 200]]
+console.log(arr2.length);
+
+for(var i = 0; i <= arr2.length-1; i++) {
+    console.log('Element at index ${i}', arr2[i]);
+}
+// console.log("Element at 0th index is: ",arr1[0]);
+// console.log("Element at 1st index is: ",arr1[1]);
+// console.log("Element at 2nd index is: ",arr1[2]);
+// console.log("Element at 3rd index is: ",arr1[3]);
---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+//      Primitive data types
+// number data type: all num (real, whole, decimal)
+// their z no fixed size r range like java data types
+var a = 90.7812234567;
+console.log(a, typeof a);
+
+// Strings: single char r group of char all are strings
+var b = "k"
+console.log(b, typeof b);
+
+// boolean: true / false
+var c = true;
+console.log(c, typeof c);
+
+// undefined: 
+let d;
+console.log(d);
+
+// null:
+let e = null;
+console.log(e);
